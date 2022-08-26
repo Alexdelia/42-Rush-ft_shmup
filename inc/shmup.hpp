9b77293edffd3ca130db49210a330eff35665016
Alexdelia <alexandre.delille.57@gmail.com>
2022-08-26 23:20:14 +0200

    `main.cpp` + `shmup.hpp`

---
+++ inc/shmup.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shmup.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 21:52:52 by adelille          #+#    #+#             */
+/*   Updated: 2022/08/26 22:51:30 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHMUP_HPP
+# define SHMUP_HPP
+
+# include <ncurses.h>
+# include <iostream>
+# include <string>
+# include <vector>
+
+# include "env.hpp"
+# include "graphic.hpp"
+# include "score.hpp"
+
+#endif
